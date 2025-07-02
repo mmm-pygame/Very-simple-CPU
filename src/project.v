bbe3824285582f9126d8e91b0dcff81224bba80c
min-ng.d <nguyendoanminh2508@gmail.com>
2025-07-02 16:03:49 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -19,7 +19,7 @@ module tt_um_asiclab_example (
     wire reset  = ~rst_n;
     assign uio_out = 0;
     assign uio_oe = 0;
-    wire_unuse = &{ena,uio_in,1'b0};
+    wire _unuse = &{ena,uio_in,1'b0};
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             uo_out <= 0;
